836fe313043de35dae383a490def7a281332fc96
orafamoura <raphaelmvb2@gmail.com>
2024-01-15 18:10:32 -0300

    Atualizacao de metodos no ProductController e ProductControllerTest

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,10 +1,12 @@
 package controller;
 
 import model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import service.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,24 +17,26 @@ import java.util.Optional;
 @SpringBootConfiguration
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
     @GetMapping
     public ResponseEntity<List<Product>> FindAll(){
 
-        List<Product> products = new ArrayList<Product>();
+        List<Product> products = productService.FindAll();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Product>> FindById(@PathVariable Long id){
 
-        Optional<Product> product = Optional.of(new Product());
-        return new ResponseEntity<>(product, HttpStatus.BAD_REQUEST);
+        Optional<Product> product = productService.FindById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product){
 
-        return new ResponseEntity<>(product, HttpStatus.OK);
+        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
     }
 
 
--- src/test/java/controller/ProductControllerTest.java
+++ src/test/java/controller/ProductControllerTest.java
@@ -1,18 +1,23 @@
 package controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import service.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @WebMvcTest
 public class ProductControllerTest {
@@ -22,6 +27,9 @@ public class ProductControllerTest {
 
     @Autowired
     private ProductController productController;
+
+    @MockBean
+    private ProductService productService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,6 +46,7 @@ public class ProductControllerTest {
         //Arrange
         List<Product> products = new ArrayList<Product>();
         var requestBuilder = MockMvcRequestBuilders.get("/api/products");
+        Mockito.when(this.productService.FindAll()).thenReturn(products);
 
         //Act
         this.mockMvc.perform(requestBuilder)
@@ -46,6 +55,51 @@ public class ProductControllerTest {
         .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
-//testando git 
+    @Test
+    public void deve_retornar_o_produto_por_id() throws Exception{
+        //Arrange
+        Product product = new Product();
+        product.setId(2L);
+        product.setName("Martelo");
+        product.setQuantity(10);
+
+        Optional<Product> optProduct = Optional.of(product);
+
+        var requestBuilder = MockMvcRequestBuilders.get("/api/products/2");
+
+        Mockito.when(this.productService.FindById(2L)).thenReturn(optProduct);
+
+        //Act
+        this.mockMvc.perform(requestBuilder)
+        //Assert
+        .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Martelo"));
+    }
+
+    @Test
+    public void deve_adicionar_um_produto() throws Exception{
+        //Arrange
+        //criando o product
+        Product product = new Product();
+        product.setName("Martelo");
+        product.setQuantity(10);
+
+        //convertendo o product para json
+        String json = new ObjectMapper().writeValueAsString(product);
+
+        //criando a request do tipo post
+        var requestBuilder = MockMvcRequestBuilders.post("/api/products")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        //adicionando o id ao product
+        product.setId(1l);
+        Mockito.when(this.productService.addProduct(product)).thenReturn(product);
+
+        //Act
+        this.mockMvc.perform(requestBuilder)
+                //Assert
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
 
 }
